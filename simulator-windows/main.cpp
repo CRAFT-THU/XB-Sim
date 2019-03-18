57c1c717ba6dbbc146c0ea58226786e37b1ae879
He Liu <liuhe94@hotmail.com>
2019-03-19 00:30:32 +0800

    modify conv and main

--- simulator-windows/main.cpp
+++ simulator-windows/main.cpp
@@ -52,26 +52,26 @@ int sc_main(int argc, char* argv[]) {
 	sc_signal<int> signal_38;
 	// int signal_counter = 0;
 
-	sc_signal<float> send_data1[INPUT_SIZE*CHANNELS_3];
-	sc_signal<float> conv_1_out[CHANNELS_32];
-	sc_signal<float> conv_2_out[CHANNELS_32];
-	sc_signal<float> conv_3_out[CHANNELS_32];
-	sc_signal<float> conv_4_out[CHANNELS_48];
-	sc_signal<float> conv_5_out[CHANNELS_48];
-	sc_signal<float> conv_6_out[CHANNELS_80];
-	sc_signal<float> conv_7_out[CHANNELS_80];
-	sc_signal<float> conv_8_out[CHANNELS_80];
-	sc_signal<float> conv_9_out[CHANNELS_80];
-	sc_signal<float> conv_10_out[CHANNELS_80];
-	sc_signal<float> conv_11_out[CHANNELS_128];
-	sc_signal<float> conv_12_out[CHANNELS_128];
-	sc_signal<float> conv_13_out[CHANNELS_128];
-	sc_signal<float> conv_14_out[CHANNELS_128];
-	sc_signal<float> conv_15_out[CHANNELS_128];
-	sc_signal<float> linear_1_out[INPUT_LINEAR_2];
-	sc_signal<float> linear_2_out[INPUT_LINEAR_3];
-	sc_signal<float> linear_3_out[INPUT_LINEAR_4];
-	sc_signal<float> linear_4_out[OUTPUT_LINEAR];
+	sc_signal<float> *send_data1 = new sc_signal<float>[INPUT_SIZE*CHANNELS_3];
+	sc_signal<float> *conv_1_out = new sc_signal<float>[CHANNELS_32];
+	sc_signal<float> *conv_2_out = new sc_signal<float>[CHANNELS_32];
+	sc_signal<float> *conv_3_out = new sc_signal<float>[CHANNELS_32];
+	sc_signal<float> *conv_4_out = new sc_signal<float>[CHANNELS_48];
+	sc_signal<float> *conv_5_out = new sc_signal<float>[CHANNELS_48];
+	sc_signal<float> *conv_6_out = new sc_signal<float>[CHANNELS_80];
+	sc_signal<float> *conv_7_out = new sc_signal<float>[CHANNELS_80];
+	sc_signal<float> *conv_8_out = new sc_signal<float>[CHANNELS_80];
+	sc_signal<float> *conv_9_out = new sc_signal<float>[CHANNELS_80];
+	sc_signal<float> *conv_10_out = new sc_signal<float>[CHANNELS_80];
+	sc_signal<float> *conv_11_out = new sc_signal<float>[CHANNELS_128];
+	sc_signal<float> *conv_12_out = new sc_signal<float>[CHANNELS_128];
+	sc_signal<float> *conv_13_out = new sc_signal<float>[CHANNELS_128];
+	sc_signal<float> *conv_14_out = new sc_signal<float>[CHANNELS_128];
+	sc_signal<float> *conv_15_out = new sc_signal<float>[CHANNELS_128];
+	sc_signal<float> *linear_1_out = new sc_signal<float>[INPUT_LINEAR_2];
+	sc_signal<float> *linear_2_out = new sc_signal<float>[INPUT_LINEAR_3];
+	sc_signal<float> *linear_3_out = new sc_signal<float>[INPUT_LINEAR_4];
+	sc_signal<float> *linear_4_out = new sc_signal<float>[OUTPUT_LINEAR];
 
 	sc_signal<float> buff_1_out[CHANNELS_32][INPUT_SIZE];
 	sc_signal<float> buff_2_out[CHANNELS_32][INPUT_SIZE];
@@ -87,10 +87,10 @@ int sc_main(int argc, char* argv[]) {
 	sc_signal<float> buff_12_out[CHANNELS_128][INPUT_SIZE];
 	sc_signal<float> buff_13_out[CHANNELS_128][INPUT_SIZE];
 	sc_signal<float> buff_14_out[CHANNELS_128][INPUT_SIZE];
-	sc_signal<float> buff_linear_1_out[INPUT_LINEAR_1];
-	sc_signal<float> buff_linear_2_out[INPUT_LINEAR_2];
-	sc_signal<float> buff_linear_3_out[INPUT_LINEAR_3];
-	sc_signal<float> buff_linear_4_out[INPUT_LINEAR_4];
+	sc_signal<float> *buff_linear_1_out = new sc_signal<float>[INPUT_LINEAR_1];
+	sc_signal<float> *buff_linear_2_out = new sc_signal<float>[INPUT_LINEAR_2];
+	sc_signal<float> *buff_linear_3_out = new sc_signal<float>[INPUT_LINEAR_3];
+	sc_signal<float> *buff_linear_4_out = new sc_signal<float>[INPUT_LINEAR_4];
 	
 	// number generator
 	numgen Num_gen("number_generator");
